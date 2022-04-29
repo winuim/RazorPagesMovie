da34aa3ad31592ac166b6f30a6c18548a9199614
yohei <uema.yohei@outlook.jp>
2022-04-30 01:23:03 +0900

    パート6 検索の追加

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,10 +1,11 @@
-#nullable disable
+﻿#nullable disable
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -20,11 +21,32 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchingString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchingString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchingString));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
